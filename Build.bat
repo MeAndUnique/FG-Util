aacaabcc95178a94601856461e48909e8f3ddafe
Shane Parker <shane.tim.parker@gmail.com>
2021-02-02 20:11:36 +0000

    Updated build script to better fit new project structure.

--- Build.bat
+++ Build.bat
@@ -3,18 +3,20 @@
 rem get file name
 for %%I in (.) do set FileName=%%~nxI
 
+rem get target directory
+for /d %%d in (*.*) do (
+    if %%d==extension (set Target=%%d)
+    if %%d==module (set Target=%%d)
+)
+
 rem get extension
-pushd ..
-for %%I in (.) do set DirName=%%~nxI
-if %DirName%==extensions (set FileExt=ext)
-if %DirName%==modules (set FileExt=mod)
-echo %FileExt%
-popd
+if %Target%==extension (set FileExt=ext)
+if %Target%==module (set FileExt=mod)
 
 set FileName=%FileName%.%FileExt%
 echo %FileName%
 
 del source.zip
-"D:\Program Files\7-Zip\7z.exe" a source.zip .\source\*
+"D:\Program Files\7-Zip\7z.exe" a source.zip .\%Target%\*
 del %FileName%
 ren source.zip %FileName%
